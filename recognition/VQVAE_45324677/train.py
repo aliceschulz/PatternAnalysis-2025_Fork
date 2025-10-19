b0032d5e8b9cd15c2f470f88b5acaaebd6c7ca7e
aliceschulz <a.h.schulz@hotmail.com>
2025-10-19 12:35:10 +1000

    feat: Preliminary training loop created.

--- recognition/VQVAE_45324677/train.py
+++ recognition/VQVAE_45324677/train.py
@@ -6,6 +6,7 @@
 import torch.optim as optim
 from modules.py import VQVAE_Model
 from dataset.py import training_loader, test_loader, validation_loader
+from tqdm import tqdm
 
 # Hyperparameters
 num_epochs = 2
@@ -37,5 +38,33 @@ def full_VQVAE_loss(VQ_loss, x_reconstructed, inputs):
     VQVAE_loss = reconstruction_loss + VQ_loss
     return VQVAE_loss
 
+# Training loop
+for epoch in range(num_epochs): 
+    VQVAE_Model.train()  # Sets the model into training mode.
+    training_loss = 0.0  # initialise training loss
 
+    # Use tqdm to create an "iterable object that acts exactly
+    # like the original iterable, but prints a dynamically updating
+    # progress bar every time a value is requested."
+    progress_bar = tqdm(enumerate(training_loader), 
+                        total=len(training_loader), 
+                        desc=f'Epoch {epoch+1}/{num_epochs}')
+    
+    for batch_idx, (inputs, _) in progress_bar:
 
+        inputs = inputs.to(device)
+        # calculate loss, backward propagate, then have optimiser take a step
+        optimiser.zero_grad()
+        loss, data_reconstructed  = VQVAE_Model(inputs)
+        loss = full_VQVAE_loss(loss, data_reconstructed, inputs)
+        loss.backward()
+        training_loss += loss.item()
+        optimiser.step()
+
+        # Update tqdm description with current loss
+        progress_bar.set_postfix({'Loss': loss.item()})
+
+    avg_loss = training_loss / len(training_loader.dataset)
+    print(f'Epoch [{epoch + 1}/{num_epochs}], 
+          Average Loss: {avg_loss:.4f}, 
+          Training loss: {training_loss}')
