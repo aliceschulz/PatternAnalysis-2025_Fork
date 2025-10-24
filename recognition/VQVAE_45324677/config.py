f0c472c89a32f8566f0634c3634bb2f7136abae7
aliceschulz <a.h.schulz@hotmail.com>
2025-10-24 14:54:01 +1000

    feat: Separate config file for defining constants and hyperparameters.

---
+++ recognition/VQVAE_45324677/config.py
@@ -0,0 +1,14 @@
+# Configuration file
+# Specifies specific constants 
+
+#######################
+##### plotting.py #####
+#######################
+
+plot_save_path = "/home/Student/s4532467/plots"
+
+####################
+##### train.py #####
+####################
+
+visualise_every = 3
